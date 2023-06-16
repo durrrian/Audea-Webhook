500589a3735d5dcb6c1fd5436ecc46ebd011476d
Furqon Wilogo <69423972+fdwilogo@users.noreply.github.com>
2023-06-16 12:06:23 +0000

    Update 2 files

--- src/app/api/v1/clerkUpdatedProfile/route.ts
+++ src/app/api/v1/clerkUpdatedProfile/route.ts
@@ -1,88 +1,90 @@
 import { mongo } from "@/utils/mongo";
 import { notionInternal } from "@/utils/notion";
 import type { WebhookEvent } from "@clerk/clerk-sdk-node";
-import { NextRequest, NextResponse } from "next/server";
+import { ObjectId } from "mongodb";
+import { NextResponse } from "next/server";
 
-export async function POST(request: NextRequest) {
-	try {
-		await mongo.connect();
+export async function POST(request: Request) {
+  try {
+    await mongo.connect();
 
-		const database = mongo.db("app-v1");
-		const collection = database.collection("User");
+    const database = mongo.db("app-v1");
+    const collection = database.collection("User");
 
-		const body = request.body as any;
-		const evt = body.evt as WebhookEvent;
+    const req = (await request.json()) as WebhookEvent;
 
-		if (evt.type === "user.updated") {
-			const { data } = evt;
+    if (req.type === "user.updated") {
+      const { data } = req;
 
-			const user = await collection.findOne({ clerkUserId: data.id });
+      const user = await collection.findOne({ clerkUserId: data.id });
 
-			if (!user) return NextResponse.error();
+      if (!user) return NextResponse.error();
 
-			const email = data.email_addresses.find(
-				({ id }) => id === data.primary_email_address_id
-			)?.email_address;
+      const email = data.email_addresses.find(
+        ({ id }) => id === data.primary_email_address_id
+      )?.email_address;
 
-			//it is impossible that a user can have no email
-			if (!email) return NextResponse.error();
+      //it is impossible that a user can have no email
+      if (!email) return NextResponse.error();
 
-			const response = await collection.updateOne(
-				{ _id: user._id },
-				{
-					email,
-					firstName: data.first_name,
-					lastName: data.last_name,
-				}
-			);
+      const response = await collection.updateOne(
+        { _id: new ObjectId(user._id) },
+        {
+          $set: {
+            email,
+            firstName: data.first_name,
+            lastName: data.last_name,
+          },
+        }
+      );
 
-			await notionInternal.pages.update({
-				page_id: user.notionPageId,
-				properties: {
-					Name: {
-						title: [
-							{
-								text: {
-									content: `${data.first_name} ${data.last_name}`,
-								},
-							},
-						],
-					},
+      await notionInternal.pages.update({
+        page_id: user.notionPageId,
+        properties: {
+          Name: {
+            title: [
+              {
+                text: {
+                  content: `${data.first_name} ${data.last_name}`,
+                },
+              },
+            ],
+          },
 
-					Email: {
-						email,
-					},
+          Email: {
+            email,
+          },
 
-					"First Name": {
-						rich_text: [
-							{
-								text: {
-									content: data.first_name,
-								},
-							},
-						],
-					},
+          "First Name": {
+            rich_text: [
+              {
+                text: {
+                  content: data.first_name,
+                },
+              },
+            ],
+          },
 
-					"Last Name": {
-						rich_text: [
-							{
-								text: {
-									content: data.last_name,
-								},
-							},
-						],
-					},
-				},
-			});
+          "Last Name": {
+            rich_text: [
+              {
+                text: {
+                  content: data.last_name,
+                },
+              },
+            ],
+          },
+        },
+      });
 
-			return NextResponse.json(response);
-		} else {
-			return NextResponse.error();
-		}
-	} catch (error) {
-		console.error(error);
-		return NextResponse.error();
-	} finally {
-		await mongo.close();
-	}
+      return NextResponse.json(response);
+    } else {
+      return NextResponse.error();
+    }
+  } catch (error) {
+    console.error(error);
+    return NextResponse.error();
+  } finally {
+    await mongo.close();
+  }
 }
